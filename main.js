0aa56245524a62c3d3fc64eae0405dbf64106882
varnikap <78656240+varnikap@users.noreply.github.com>
2021-11-05 21:00:38 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,19 +5,37 @@ function preload()
 
 function setup()
 {
-     createCanvas(640,380);
+     canvas = createCanvas(300,300);
+     canvas.center();
+     video = createCapture(VIDEO);
+     video.size(300,300);
+     video.hide();
 
-
-
+poseNet= ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+function modelLoaded()
+{
+    console.log("PoseNet is Initialized");
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+    }
 }
 
 function draw()
 {
-    background(255);
+    image(video, 0, 0, 300, 300);
+
 
 }
 
 function takesnapshot()
 {
-
+save('myFilterImage.png');
 }
