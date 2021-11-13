bb450952ae1c4e90fc3327b1f918aed8e738d7ca
varnikap <78656240+varnikap@users.noreply.github.com>
2021-11-12 20:45:17 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+noseX=0;
+noseY=0;
+
+
 function preload()
 {
-
+clown_nose = loadImage('https://i.postimg.cc/cCSMc6dv/unnamed.png');
 }
 
 function setup()
@@ -14,6 +18,7 @@ function setup()
 poseNet= ml5.poseNet(video,modelLoaded);
 poseNet.on('pose',gotPoses);
 }
+
 function modelLoaded()
 {
     console.log("PoseNet is Initialized");
@@ -23,8 +28,10 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+       noseX = results[0].pose.nose.x -15;
+       noseY = results[0].pose.nose.y -  -20;
+       console.log("nose x = " + noseX);
+       console.log("nose y = " + noseY);
     }
 }
 
@@ -32,6 +39,7 @@ function draw()
 {
     image(video, 0, 0, 300, 300);
 
+    image(clown_nose, noseX,noseY, 30, 30);
 
 }
 
